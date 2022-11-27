6cc20edc820bf878309725eaf42562435622b5e2
Emilly <110494320+Emillyzinha@users.noreply.github.com>
2022-11-27 02:20:06 -0300

    Add files via upload

---
+++ SITE FINAL/carrinho.js
@@ -0,0 +1,84 @@
+listaProdutos = []
+
+function addCarrinho(){
+    var notebook = {nome: "Notebook Acer Swift 3 SF314-57-57VY Intel Core I5 16GB 256GB SSD 14' Windows 10", valor:"2095.60"}
+
+    listaProdutos.push(notebook)
+    console.log(listaProdutos)
+    
+    alert("Seu produto foi adicionado no carrinho com sucesso!")
+
+    exibirProdutos()
+    salvarCarrinho()
+}
+
+function exibirProdutos(){
+    var codHtml = ''
+    var index = 0
+    var qtd = 0
+
+    listaProdutos.forEach(item => {
+        qtd+=1
+        // codHtml = `<div class= "teste1"><p>${item.nome} <br> (${qtd}) </p><b> <a href="javascript:void(0)"onclick="deletar(${index})"><img src="./imagens3/cancelar.png" alt=""></a></b></div>`
+        codHtml += `<div class= "teste1"><img class="imagemNotebook" src="images copy/notebook completa.png" alt=""/><p>${item.nome} <br> (${qtd}) </p><b> <a href="javascript:void(0)"onclick="deletar(${index})"><img class="paraExcluir" src="./images copy/iconExcluir.png" alt=""></a></b></div>`
+        
+    })
+
+    index = 1
+
+    if (document.getElementById('localNotebook') != null){
+        document.getElementById('localNotebook').innerHTML = codHtml
+    }
+    // if (document.getElementById('produtosM') != null){
+    //     document.getElementById('produtosM').innerHTML = codHtml
+    // }
+
+    total()
+}
+
+function total(){
+    var total = 0
+    var printTotal = ''
+    var parcela = 0
+    listaProdutos.forEach(item => {
+        valor =  parseInt(item.valor);
+        total += valor
+        parcela = valor/4
+        printTotal = `<div><h1>R$${total},00</h1><p>ou 4x de R$${parcela}</p></div>`
+        
+    });
+
+    if(total == 0){
+        document.getElementById('valorTotal').innerHTML ='<h2>Seu carrinho está vazio</h2>'
+        document.getElementById('valorTotal').innerHTML ='<h2>Seu carrinho está vazio</h2>'
+    }
+    else{
+        if(document.getElementById('valorTotal') != null){
+            document.getElementById('valorTotal').innerHTML = printTotal
+        }
+        // if(document.getElementById('valorTotal') != null){
+        //     document.getElementById('valorTotal').innerHTML = printTotal
+        // }
+
+    }
+    
+    // total()
+}
+function deletar(index1){
+    listaProdutos.splice(index1, 1)
+    salvarCarrinho()
+    obterCarrinho()
+}
+
+function salvarCarrinho(){
+    localStorage.setItem('loja',JSON.stringify(listaProdutos))
+}
+function obterCarrinho(){
+    if (localStorage.getItem('loja')!= null){
+        listaProdutos = JSON.parse(localStorage.getItem('loja')) 
+        exibirProdutos()
+    }
+  
+}
+
+obterCarrinho()

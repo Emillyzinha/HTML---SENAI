d8c92e9205021705efb41cd8fd55e0b12899ba60
Emilly <110494320+Emillyzinha@users.noreply.github.com>
2022-12-06 16:37:18 -0300

    Delete SITE FINAL directory

--- SITE FINAL/voltar.js
+++
@@ -1,9 +0,0 @@
-var btn = document.querySelector("#voltarinicio");
-btn.addEventListener("click", function() {
-    window.scrollTo(0, 0);
-});
-
-var btn = document.querySelector("#voltarinicioMobile");
-btn.addEventListener("click", function() {
-    window.scrollTo(0, 0);
-});
